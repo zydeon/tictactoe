1646fa31b2b33affa820877eadcb485ae2a0ddc4
Ricardo Santos <rrsantos@student.dei.uc.pt>
2013-04-22 23:19:36 +0100

    Jogo a funcionar e com teste

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,14 +1,8 @@
-#import "TicTacToe.hpp"
-
-TicTacToe::TicTacToe() {
-	int i;
-	for(i = 0; i < POSITIONS_NO; i++) {
-		positions[i] = NO_MOVE;
-	}
-	xTurn = true;
-}
+#include "TicTacToe.hpp"
 
 int TicTacToe::play(bool _xTurn, int pos) {
+	bool win;
+	
 	if(!isRightTurn(_xTurn)) {
 		return INVALID_TURN;
 	}
@@ -19,7 +13,20 @@ int TicTacToe::play(bool _xTurn, int pos) {
 	
 	placeMove(_xTurn,pos);
 	
-	return won(_xTurn) ? WIN : NO_WIN;
+	win = won(_xTurn);
+	xTurn = !xTurn;
+	
+	if(win) {
+		resetGame();
+		return WIN;
+	}
+	else if(isFull()){
+		resetGame();
+		return OVER;
+	}
+	else {
+		return NO_WIN;
+	}
 }
 
 bool TicTacToe::isValidMove(int pos) {
@@ -38,13 +45,68 @@ bool TicTacToe::won(bool _xTurn) {
 	int i, aux;
 	
 	aux = _xTurn ? X_MOVE : O_MOVE;
+	int a1,a2,a3;
 	
 	for(i = 0; i < WIN_CASES_NO; i++) {
-		if(aux == WIN_CASES[i][0] && aux == WIN_CASES[i][1] 
-				&& aux == WIN_CASES[i][2]) {
+		a1 = positions[win_cases[i][0]];
+		a2 = positions[win_cases[i][1]];
+		a3 = positions[win_cases[i][2]];
+		if(aux == a1 && aux == a2 && aux == a3) {
 			return true;
 		}
 	}
 	
 	return false;
+}
+
+bool TicTacToe::isFull() {
+	int i,count;
+	for(i = count = 0; i< POSITIONS_NO; i++) {
+		if(positions[i] != NO_MOVE) {
+			count ++;
+		}
+	}
+	return count == POSITIONS_NO;
+}
+
+void TicTacToe::resetGame() {
+	int i;
+	for(i = 0; i < POSITIONS_NO; i++) {
+		positions[i] = NO_MOVE;
+	}
+	printf("Game Reset\n");
+}
+
+TicTacToe::TicTacToe() {
+	int i;
+	for(i = 0; i < POSITIONS_NO; i++) {
+		positions[i] = NO_MOVE;
+	}
+	xTurn = true;
+}
+
+void TicTacToe::plotGame() {
+	int i,j;
+	char aux;
+	i = 0;
+	while(i < POSITIONS_NO) {
+		for(j = 0; j < 3; j++) {
+			switch(positions[i++]) {
+				case NO_MOVE: aux = ' '; break;
+				case X_MOVE: aux = 'X'; break;
+				case O_MOVE: aux = 'O'; break;
+			}
+			printf("|%c|",aux);
+		}
+		printf("\n----------\n");
+	}
+	printf("\n");
+}
+
+bool TicTacToe::isXTurn() {
+	return xTurn;
+}
+
+int *TicTacToe::getPositions() {
+	return positions;
 }
--- TicTacToe.hpp
+++ TicTacToe.hpp
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #define NO_MOVE 0
 #define X_MOVE 1
 #define O_MOVE 2
@@ -5,18 +7,14 @@
 #define INVALID_TURN -2
 #define WIN 3
 #define NO_WIN 4
+#define OVER 5
 
 #define POSITIONS_NO 9
 
 #define WIN_CASES_NO 8
-#define WIN_CASES { { 0, 1, 2 },
-					{ 3, 4, 5 },
-					{ 6, 7, 8 },
-					{ 0, 3, 6 },
-					{ 1, 4, 7 },
-					{ 2, 5, 8 },
-					{ 0, 4, 8 },
-					{ 2, 4, 6 } }
+
+
+static int win_cases[8][3] = {{ 0, 1, 2 }, { 3, 4, 5 }, {6, 7, 8 }, {0, 3, 6 },{ 1, 4, 7 },{ 2, 5, 8 },{ 0, 4, 8 },{ 2, 4, 6 }};
 
 class TicTacToe {
 	
@@ -24,8 +22,11 @@ class TicTacToe {
 		TicTacToe();
 		int play(bool _xTurn, int pos);
 		void plotGame();
+		bool isXTurn();
+		int *getPositions();
 		
 	private:
+		
 		int positions[POSITIONS_NO];
 		bool xTurn;
 	
@@ -33,4 +34,6 @@ class TicTacToe {
 		bool isValidMove(int pos);
 		void placeMove(bool _xTurn, int pos);
 		bool won(bool _xTurn);
+		bool isFull();
+		void resetGame();
 };
