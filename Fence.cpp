f18fcf273c625ab3d77160b209d4fb23923bc822
Ricardo Santos <rrsantos@student.dei.uc.pt>
2013-05-14 18:20:28 +0100

    Cerca

---
+++ Fence.cpp
@@ -0,0 +1,87 @@
+#include "Fence.hpp"
+
+Fence::Fence(GLfloat w_, GLfloat h_, GLfloat d_) {
+	w = w_;
+	h = h_;
+	d = d_;
+}
+
+void Fence:drawCorner(GLfloat length) {
+	
+}
+
+void Fence::drawFence() {
+	glColor3f(1.0, 0.0, 1.0);
+	
+	// Front
+	glPushMatrix();
+	glScalef(3,3,3);
+	glBegin(GL_POLYGON);
+		glNormal3f(-0.5, 0.0, 1.0);
+		glVertex3f(-0.5, 0.0, 0.0);
+		glVertex3f(-0.5,	0.0, 0.0);
+		glVertex3f(-0.5, 3, 0.0);
+		glVertex3f(0, 4, 0.0);
+		glVertex3f(0.5, 3, 0.0);
+		glVertex3f(0.5, 0.0, 0.0);
+	glEnd();
+	
+	glPopMatrix();
+	
+	// Front
+	glBegin(GL_POLYGON);
+		glNormal3f(-w/2, 0.0, 1.0);
+		glVertex3f(-w/2, 0.0, 0.0);
+		glVertex3f(-w/2,	0.0, 0.0);
+		glVertex3f(-w/2, 3*h/4, 0.0);
+		glVertex3f(0, h, 0.0);
+		glVertex3f(w/2, 3*h/4, 0.0);
+		glVertex3f(w/2, 0.0, 0.0);
+	glEnd();
+	
+	// Right
+	/*glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_POLYGON);
+		glNormal3f(0.0, 0.0, 1.0);
+		glVertex3f(w,	0.0,	0.0);
+		glVertex3f(w,	0.0,	d);
+		glVertex3f(w,	3*h/4,	d);
+		glVertex3f(w/2,	h,		d);
+		glVertex3f(w/2, h,		0.0);
+		glVertex3f(w,	3*h/4,	0.0);
+		glVertex3f(w,	0.0,	0.0);
+	glEnd();*/
+	
+	// Back
+	/*glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(0.0, 0.0, d);
+		glVertex3f(w,	0.0, d);
+		glVertex3f(w, 3*h/4, d);
+		glVertex3f(w/2, h, d);
+		glVertex3f(0.0, 3*h/4, d);
+		glVertex3f(0.0, 0.0, d);
+	glEnd();*/
+	
+	// Left
+	/*glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(0.0,	0.0,	0.0);
+		glVertex3f(0.0,	0.0,	d);
+		glVertex3f(0.0,	3*h/4,	d);
+		glVertex3f(w/2,	h,		d);
+		glVertex3f(w/2, h,		0.0);
+		glVertex3f(0.0,	3*h/4,	0.0);
+		glVertex3f(0.0,	0.0,	0.0);
+	glEnd();*/
+	
+	// Bottom
+	/*glColor3f(0.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(0.0, 0.0, 0.0);
+		glVertex3f(w, 0.0, 0.0);
+		glVertex3f(w, 0.0, d);
+		glVertex3f(0.0, 0.0, d);
+	glEnd();*/
+	
+}
---
+++ test.c
@@ -0,0 +1,47 @@
+#ifdef __linux__
+	#define VERSION 0
+#elif defined __APPLE__
+	#define VERSION 1
+#else
+	#define VERSION 2
+#endif
+
+#include "TicTacToe.hpp"
+
+TicTacToe t;
+
+void handleResult(int);
+
+int main() {
+	char aux;
+	int pos;
+	bool x;
+	int y = VERSION;
+	printf("Version = %d\n",y);
+	
+	while(true) {
+		scanf("%c %d",&aux,&pos);
+		if(aux == 'x' || aux == 'X' || aux == 'o' || aux == 'O') {
+			x = (aux == 'x' || aux == 'X');
+			handleResult(t.play(x,pos));
+		}
+	}
+	return 0;
+}
+
+void handleResult(int result) {
+	bool xTurn;
+	
+	switch(result) {
+		case INVALID_MOVE: printf("Invalid Move\n"); break;
+		case INVALID_TURN: printf("Invalid Turn\n"); break;
+		case WIN: {
+			xTurn = !t.isXTurn();
+			printf("%c won the game\n", xTurn ? 'X' : 'O');
+		} break;
+		case NO_WIN: printf("Normal Accepted Move\n"); break;
+		case OVER: printf("Draw\n"); break;
+	}
+	
+	t.plotGame();
+}
